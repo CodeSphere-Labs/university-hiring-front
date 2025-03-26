4cf778afd7ff3bcfa80532dd3d28079b981f3a1c
zeroqs <smthq@bk.ru>
2025-03-27 02:50:50 +0300

    ☄️ fix: fix action card types

--- src/shared/ui/ActionCard/ActionCard.tsx
+++ src/shared/ui/ActionCard/ActionCard.tsx
@@ -1,17 +1,18 @@
+import type { ButtonProps } from '@mantine/core';
 import type { ReactNode } from 'react';
 
 import { Button, Stack, Text } from '@mantine/core';
 
 import classes from './ActionCard.module.css';
 
-interface ActionCardProps {
+interface ActionCardProps extends ButtonProps {
   grow?: boolean;
   icon: ReactNode;
   title: string;
   onClick?: () => void;
 }
 
-export const ActionCard = ({ icon, title, onClick }: ActionCardProps) => {
+export const ActionCard = ({ icon, title, onClick, ...props }: ActionCardProps) => {
   return (
     <Button
       className={classes.button}
@@ -20,6 +21,7 @@ export const ActionCard = ({ icon, title, onClick }: ActionCardProps) => {
       variant='filled'
       w={{ base: '100%', sm: '48%', md: '23%' }}
       onClick={onClick}
+      {...props}
     >
       <Stack align='center' gap={5}>
         {icon}
